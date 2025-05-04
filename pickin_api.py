fe64f0987b1cff7495226629a6b33a9a0d1f76d3
minkyung01 <84274527+minkyung01@users.noreply.github.com>
2025-05-05 00:44:19 +0900

    [update] extract keywords from text
    - keyword extraction (from text)
    - add templates

--- pickin_api.py
+++ pickin_api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from pickin_service import predict_label
+from pickin_service import predict_label, extract_keywords_from_text, extract_keywords_from_sheet, extract_sentence_embeddings, calc_similarity, feedback
 from schema.prediction_request import prediction_request
+from schema.text_keyword_extraction_request import text_keyword_extraction_request
 
 router = APIRouter()
 
@@ -8,3 +9,23 @@ router = APIRouter()
 def predict(request: prediction_request):
     prediction = predict_label(request)
     return {"prediction": prediction}
+
+@router.post("/extract/keywords/text") # 텍스트 기반 키워드 추출, 채용공고/자기소개서 공통
+def extract_keywords_from_text(request: text_keyword_extraction_request):
+    return extract_keywords_from_text(request)
+
+@router.post("/extract/keywords/sheet") # 스프레드시트 기반 키워드 추출, 채용공고/자기소개서 공통
+def extract_keywords_from_sheet(request):
+    return extract_keywords_from_sheet(request)
+
+@router.post("/extract/sentence-embedding") # 텍스트 기반 문장 임베딩 추출, 채용공고/자기소개서 공통
+def extract_sentence_embeddings(request):
+    return extract_sentence_embeddings(request)
+
+@router.post("/similarity") # 문장 임베딩 기반 유사도 계산
+def calc_similarity(request):
+    return calc_similarity(request)
+
+@router.post("/feedback") # 키워드 및 유사도 기반 정량적 피드백 & 텍스트 기반 정성적 피드백
+def feedback(request):
+    return feedback(request)
--- pickin_service.py
+++ pickin_service.py
@@ -1,9 +1,14 @@
 import joblib
 import numpy as np
+from keybert import KeyBERT
+from konlpy.tag import Okt
 
 model = joblib.load("model/pickin_model.pkl")
 scaler = joblib.load("model/scaler.pkl")
 label_encoders = joblib.load("model/label_encoders.pkl")
+
+kb_model = KeyBERT(model="distiluse-base-multilingual-cased-v1")
+okt = Okt() # 형태소 분석기 초기화
 
 def predict_label(request):
     input_data = np.array([[
@@ -16,3 +21,30 @@ def predict_label(request):
     input_data_scaled = scaler.transform(input_data)
     prediction = model.predict(input_data_scaled)
     return int(prediction[0])
+
+def extract_keywords_from_text(request): # 텍스트 기반 키워드 추출, 채용공고/자기소개서 공통
+    noun_tokens = okt.nouns(request.text) # 명사 추출
+    noun_text = " ".join(noun_tokens) 
+    
+    keywords = kb_model.extract_keywords(noun_text, top_n=request.size, stop_words=None)
+    
+    cleaned_keywords = []
+    for keyword, _ in keywords: # 중복 제거
+        kw = keyword.strip()
+        if kw not in cleaned_keywords:
+            cleaned_keywords.append(kw)
+
+    return cleaned_keywords
+
+def extract_keywords_from_sheet(request): # 스프레드시트 기반 키워드 추출, 채용공고/자기소개서 공통
+    ### todo ###
+    return request
+
+def extract_sentence_embeddings(request): # 텍스트 기반 문장 임베딩 추출, 채용공고/자기소개서 공통
+    return request
+
+def calc_similarity(request): # 문장 임베딩 기반 유사도 계산
+    return request
+
+def feedback(request): # 키워드 및 유사도 기반 정량적 피드백 & 텍스트 기반 정성적 피드백
+    return request
